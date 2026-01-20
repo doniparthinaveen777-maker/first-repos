f086ee64fea281a2de18992fbed5fc9a1aae5396
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-20 09:15:27 +0530

    conversation added

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'remuru:: hi goku'
+echo 'goku:: hi remuru'
+echo 'remuru:: ela vunnav ra'
+echo 'goku:: super ga vunna ra.what about u ra?'
+echo 'remuru:: nenu inka super ga unna ra mama.
