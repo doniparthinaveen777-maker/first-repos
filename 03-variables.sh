3d381206c7581989a45e1772fb152ba806697b67
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-20 11:10:14 +0530

    changing veriables

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ PRESON1=yuji
+ PRESON2=goj
+
+
+echo "$PRESON1:: hi $PRESON2"
+echo "$PRESON2: hi $PRESON1"
+echo "$PRESON1:: ela vunnav ra"
+echo "$PRESON2:: super ga vunna ra.what about u ra?"
+echo "$PRESON1:: nenu inka super ga unna ra mama"
