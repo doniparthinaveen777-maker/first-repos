325e9022aef767190f805554503e7b86852f5cbc
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-20 09:38:27 +0530

    FIXED SYNTAX ERROR

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,4 +4,4 @@ echo 'remuru:: hi goku'
 echo 'goku:: hi remuru'
 echo 'remuru:: ela vunnav ra'
 echo 'goku:: super ga vunna ra.what about u ra?'
-echo 'remuru:: nenu inka super ga unna ra mama.
+echo 'remuru:: nenu inka super ga unna ra mama'.
