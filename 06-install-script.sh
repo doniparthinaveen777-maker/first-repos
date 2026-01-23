d54dbc366cedfe52dcee1e097fe492c7340c2866
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-23 17:37:14 +0530

    installing mysql

--- 06-install-script.sh
+++ 06-install-script.sh
@@ -7,6 +7,16 @@ then
   echo "error:: u must be in the root user."
   exit 1
 fi
+  
+  dnf install mysql -y
 
+  if [$? -ne 0 ]
+then 
+    echo "mysql installing is failure."
+
+else
+    echo "mysql installing is success."
+     
+fi 
 
  
