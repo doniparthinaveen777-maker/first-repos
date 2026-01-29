4418f7f31b146774582cdceb637841110d8bd389
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-29 16:12:37 +0530

    installing MYSQL & GIT

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -8,24 +8,49 @@ if [ $USERID -ne 0 ]
     exit 1
 fi
  
- dnf install mysql -y
+ dnf list installed mysql
+
+  if [ $? -en 0 ]
+  then 
+    dnf install mysql -y
  
 if [ $? -ne 0 ]
  then
-     echo "mysql installing...FAILURE"
-     exit 1
-else
-     echo "mysql insatlling...SUCCESS"
+      echo "mysql installing...FAILURE"
+      exit 1
+   else
+      echo "mysql insatlling...SUCCESS"
+ else 
+      echo "MYSQL is already installed"    
 
 fi
- 
- dnf install git -y
+   
 
-if [ $? -ne 0 ]
- then 
+   dnf list installed git 
+
+   if [ $? -ne 0]
+   then 
+      dnf install git -y
+      if [ $? -ne 0 ]
+     then 
     echo "git insattling...failure"
     exit 1
 else 
     echo "git insatlling...success"
 
 fi
+else
+     echo "GIT is already.. installed"
+
+    fi
+
+    if [ $? -ne 0 ]
+    then 
+    echo "installing git... failure"
+
+    exit 1
+
+    else 
+    echo "installing git... success"
+
+    fi
