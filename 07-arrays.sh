5a51ae72adcd4d149ab5350848e6d409384b8cdc
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-29 12:58:52 +0530

    adding sum

---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+ ANIME=("one piece" "jjk" "bleach")
+
+ echo "first anime of ${ANIME[0]}"
+ echo "second anime of ${ANIME[1]}"
+ ECHO "third anime of ${ANIME[2]}"
+
+ ECHO "ALL THE ANIME OF ${ANIME[@]}"
