6dd452e772bd78e60791c83f6abe959140790553
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-29 12:16:49 +0530

    removing inner first-repos floder

--- first-repos/02-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo 'remuru:: hi goku'
-echo 'goku:: hi remuru'
-echo 'remuru:: ela vunnav ra'
-echo 'goku:: super ga vunna ra.what about u ra?'
-echo 'remuru:: nenu inka super ga unna ra mama.'
