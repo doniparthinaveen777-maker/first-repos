9b91a691ffa8a9504e2e9735cbedc9fe3f7bc96e
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-29 15:28:30 +0530

    installing MYSQL

---
+++ 10-install-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ USERID -ne 0 ]
+then 
+    echo "U MUST HAVE ROOT USER FOR EXECUTING THIS SCRIPT"
+    exit 1
+fi
+ 
+ dnf install mysqll -y
+ 
+ if [ $? -ne 0 ]
+ then
+     echo "mysql installing...FAILURE"
+     exit 1
+else
+     echo "mysql insatlling...SUCCESS"
+
+fi
