728a171588831de5049a208539a8f8124e2fff34
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-29 12:47:57 +0530

    adding sum

--- 06-data-type.sh
+++ 06-data-type.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
+NUMBER1=$1
+NUMBER2=$2
 
 TIMESTAMP=$(date)
 echo "scripts execute at : $TIMESTAMP"
-echo "EDHO OKA NUMBER IVVU BRO ADD CHESTHA :"
-read $NUMBER1
-echo "INKO NUMBER CHEPPU BRO ADD CHESTHA :"
-read $NUMBER2
-read SUM=$(($NUMBER1+$NUMBER2))
+
+SUM=$(($NUMBER1+$NUMBER2))
 
 echo "ADDING sum of $NUMBER1 AND $NUMBER2 is $SUM "
