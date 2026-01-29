246eb909de799e71eb2af50a5c3618a8d9f9a9c9
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-29 15:40:36 +0530

    installing MYSQL

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -3,18 +3,29 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then 
+ then 
     echo "U MUST HAVE ROOT USER FOR EXECUTING THIS SCRIPT"
     exit 1
 fi
  
  dnf install mysqll -y
  
- if [ $? -ne 0 ]
+if [ $? -ne 0 ]
  then
      echo "mysql installing...FAILURE"
      exit 1
 else
      echo "mysql insatlling...SUCCESS"
+
+fi
+ 
+ dnf instal git -y
+
+if [ $? -ne 0 ]
+ then 
+    echo "git insattling...failure"
+    exit 1
+else 
+    echo "git insatlling...success"
 
 fi
