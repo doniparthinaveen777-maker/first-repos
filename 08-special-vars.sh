a3bdcfdc834b31f34762d6e84e71ccc61de3311c
sanjusathwik <sanjusathiwk777@gmail.com>
2026-01-29 13:49:37 +0530

    adding sum

---
+++ 08-special-vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variables passed: $@"
+echo "Number of variables: $#"
+echo "Script name: $0"
+echo "Present working directory: $pwd"
+echo "Home direcotry of current user: $HOME"
+echo "User is running this script: $USER"
+echo "Id of current Script: $$"
+sleep 60 $ # this command is used for example only. this '&' symbel is useed for run in backgruond
+echo "Id of last command in background: $!"
